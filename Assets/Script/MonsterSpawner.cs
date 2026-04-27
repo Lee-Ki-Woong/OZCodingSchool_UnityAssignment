206f028bc2dd3a20f27e52a4efdaba3e25d6627e
LeeKiWoong <littlemanner1@gmail.com>
2026-04-28 03:35:01 +0900

    [UI 생성 및 버튼 클릭 이벤트 구현, 씬 이동]OZCodingSchool_Assignment_0427
    대충 1시간 걸린 거 같다

---
+++ Assets/Script/MonsterSpawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject MonsterPrefab;
+
+    public void OnClickMonsterSpawnButton()
+    {
+        Instantiate(MonsterPrefab);
+    }
+}

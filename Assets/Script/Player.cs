5f6519344e5457f9e6506d5e79b0a606bfd4bcb9
LeeKiWoong <littlemanner1@gmail.com>
2026-04-28 19:42:45 +0900

    [메인 UI 만들기 - 1] OZCodingSchool_Assignment_0428(1)
    캔버스 나누기 방식으로 기본적인 UI 틀을 잡았다

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,71 @@
+﻿using Unity.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // [Field] ====================================================
+    #region Field
+    private Rigidbody m_playerRigidbody;
+    private GameObject m_playerGameObject;
+    private Transform m_playerTransform;
+    #endregion
+
+
+    // [Inspector Field] ====================================================
+
+    [SerializeField] private float PlayerMoveSpeed;
+    [SerializeField] private float RotateSpeed;
+
+    [SerializeField] private float JumpPoint;
+    [SerializeField] private int JumpCount;
+
+    // [Life Cycle] ====================================================
+
+    private void OnEnable()
+    {
+        GetPlayerRigidbody();
+        GetPlayerGameObject();
+        GetPlayerTransform();
+    }
+
+    private void Update()
+    {
+        PlayerMove();
+    }
+
+    // [Get] ====================================================
+
+    private void GetPlayerRigidbody()
+    {
+        m_playerRigidbody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void GetPlayerGameObject()
+    {
+        m_playerGameObject = this.gameObject;
+        Debug.Log("가나다라");
+    }
+
+    private void GetPlayerTransform()
+    {
+        m_playerTransform = this.transform;
+    }
+
+
+    // [Active] ================================================
+
+    private void PlayerMove()
+    {
+        float AD = Input.GetAxis("Horizontal");
+        float WS = Input.GetAxis("Vertical");
+
+        Vector3 moving = new Vector3(AD, 0, WS);
+
+        moving.Normalize();
+        moving = m_playerTransform.TransformDirection(moving);
+
+        m_playerRigidbody.MovePosition(m_playerRigidbody.position +  moving * PlayerMoveSpeed * Time.deltaTime);
+
+
+    }
+}
